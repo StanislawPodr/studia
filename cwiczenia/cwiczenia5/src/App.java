64f97c5036d235266cd18f7c975bbbe26baa57f6
Stanislaw Podrecki <s.podrecki@gmail.com>
2024-11-20 10:32:36 +0100

    dodanie mnożenia macierzy w wersji którą chcaił Tabakow

--- cwiczenia/cwiczenia5/src/App.java
+++ cwiczenia/cwiczenia5/src/App.java
@@ -1,5 +1,27 @@
 public class App {
+    public static int[][] multiply(int[][] macierz1, int[][] macierz2) {
+        int[][] value = new int[macierz1.length][macierz2[0].length];
+        for (int wiersz = 0; wiersz < macierz1.length; wiersz++) {
+            for (int kolumna = 0; kolumna < macierz2[0].length; kolumna++) {
+                for (int i = 0; i < macierz2.length; i++) {
+                    value[wiersz][kolumna] += macierz1[wiersz][i] * macierz2[i][kolumna];
+                }
+            }
+        }
+
+        return value;
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[][] x1 = { { 3, 1 }, { 2, 1 }, { 1, 0 } };
+        int[][] x2 = { { 1, 0, 2 }, { -1, 3, 1 } };
+        int[][] result = multiply(x1, x2);
+        for(int[] i : result) {
+            for(int j : i) {
+                System.out.print(j);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
