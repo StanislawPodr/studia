190ede99a13bf628c3b8e1da5535278fc8b5ca09
Stanislaw Podrecki <s.podrecki@gmail.com>
2025-01-17 23:53:55 +0100

    usunalem poprzednie

--- laby/lista7/src/main/java/inne/AddPracownikStrategy.java
+++
@@ -1,9 +0,0 @@
-package inne;
-
-import uczelnia.Osoba;
-import java.util.List;
-import java.util.Scanner;
-
-public interface AddPracownikStrategy {
-    public void add(List<Osoba> osoby, Scanner scanner);
-}
--- laby/lista7/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module lista7 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens lista7 to javafx.fxml;
-    exports lista7;
-}
