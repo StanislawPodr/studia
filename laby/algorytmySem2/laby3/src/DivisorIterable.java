370d07964833595f632ae8d451b7f053019e6a51
Stanislaw Podrecki <s.podrecki@gmail.com>
2025-03-17 00:30:35 +0100

    dodałem naprawę błędnego argumentu

--- laby/algorytmySem2/laby3/src/DivisorIterable.java
+++ laby/algorytmySem2/laby3/src/DivisorIterable.java
@@ -2,42 +2,39 @@ import java.util.Iterator;
 
 public class DivisorIterable implements Iterable<Integer> {
     public DivisorIterable(int number) {
-        this.number = number;
+        this.number = Math.abs(number);
         this.currentDivisor = 1;
     }
 
     private class DivisorIterator implements Iterator<Integer> {
         @Override
-    public boolean hasNext() {
-        return currentDivisor < number;
-    }
+        public boolean hasNext() {
+            return currentDivisor < number;
+        }
 
-    @Override
-    public Integer next() {
-        int halfOfNumber = number / 2;
-        while (currentDivisor <= halfOfNumber) {
-            if (number % currentDivisor == 0) {
-                return currentDivisor++;
+        @Override
+        public Integer next() {
+            int halfOfNumber = number / 2;
+            while (currentDivisor <= halfOfNumber) {
+                if (number % currentDivisor == 0) {
+                    return currentDivisor++;
+                }
+                currentDivisor++;
             }
-            currentDivisor++;
+
+            if (currentDivisor == halfOfNumber + 1) {
+                currentDivisor = number;
+                return number;
+            }
+
+            throw new IllegalStateException("No more divisors");
         }
-
-        if(currentDivisor == halfOfNumber + 1) {
-            currentDivisor = number;
-            return number;
-        }
-
-        throw new IllegalStateException("No more divisors");
     }
-    }
-
-    
 
     @Override
     public Iterator<Integer> iterator() {
-       return new DivisorIterator();
+        return new DivisorIterator();
     }
-
 
     private int number;
     private int currentDivisor;
