0673588c77a9e295319fd893cb9cc07598858ef8
Stanislaw <s.podrecki@gmail.com>
2025-11-12 18:27:21 +0100

    laby 5 z paradygmatów. Zabawa z typami

---
+++ laby/paradygmatySem3/laby5/main.ml
@@ -0,0 +1,26 @@
+type code = Elem of bool | Not | And | Or | Xor 
+
+let eval (arg: code list) = 
+  if arg = [] then raise (invalid_arg "Empty list")
+  else let rec evalRec elemLst stack = 
+    match (stack, elemLst) with 
+      ((Elem hd):: tl, _)-> evalRec (hd::elemLst) tl
+      |((Not) :: tl, a::b) -> evalRec ((not a)::b) tl 
+      |((And) :: tl, a::b::c) -> evalRec ((a && b)::c) tl
+      |((Or) :: tl, a::b::c) -> evalRec ((a || b)::c) tl
+      |((Xor) :: tl, a::b::c) -> evalRec ((a <> b)::c) tl
+      |([], [x]) -> x
+      |_ -> raise (invalid_arg "wrong stack")
+  in evalRec [] arg;;
+
+
+eval [Elem true; Elem false; And; Not];;
+eval [Elem true; Elem false; Elem true; Not; And; Not; And];;
+eval [Elem true; Elem false; And;];;
+eval [Elem true; Elem false; And; And];;
+
+
+      
+          
+      
+    
---
+++ laby/probabilitySem3/laby3/laby3.R
@@ -0,0 +1,11 @@
+#zadanie 1
+rzutMoneta5Reszek <- dbinom(5, 6, 0.5)
+reszki3LubWiecej <- 1 - pbinom(2, 6, 0.5)
+od2Do4Reszek <- pbinom(4, 6, 0.5) - pbinom(1, 6, 0.5)
+plot(0:6, sapply(0:6, function(x) dbinom(x, 6, 0.5)), type = "h")
+
+#zadanie2
+dpois(5, 2)
+1 - ppois(3, 2)
+ppois(5, 2) - ppois(2, 2)
+plot(0:30, sapply(0:30, function(x) dpois(x, 2)), type="h")
