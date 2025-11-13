b5217a0d6ec8403862326b12f8a4157f23e4c1a0
Stanislaw <s.podrecki@gmail.com>
2025-11-13 18:55:48 +0100

    laby 3 z prawdopodobienstwa

--- laby/probabilitySem3/laby3/laby3.R
+++ laby/probabilitySem3/laby3/laby3.R
@@ -11,5 +11,29 @@ ppois(5, 2) - ppois(2, 2)
 plot(0:30, sapply(0:30, function(x) dpois(x, 2)), type="h")
 
 #zadanie3
-dunif(5, 4, 12)
-punif()
+punif(7, 4, 12)
+punif(11, 4, 12) - punif(5, 4, 12)
+1 - punif(10, 4, 12)
+qunif(0.4, 4, 12)
+
+
+#zadanie4
+1 - pexp(0.5, 4)
+pexp(1/3, 4)
+pexp(80/60, 4) - pexp(2/3, 4)
+qexp(0.8, 4)
+plot((0:300)/100, sapply((0:300)/100, function(x) dexp(x, 4)))
+
+
+#zadanie5
+1 - pnorm(180, 170, 12)
+pnorm(165, 170, 12)
+pnorm(190, 170, 12) - pnorm(150, 170, 12)
+qnorm(0.9, 170, 12)
+
+
+#zadanie6
+dbinom(27, 180, 1/6)
+1 - pbinom(31, 180, 1/6)
+pbinom(28, 180, 1/6)
+pbinom(33, 180, 1/6) - pbinom(24, 180, 1/6)
